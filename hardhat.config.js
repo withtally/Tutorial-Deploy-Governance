19b14d47e62a9e78be48f4796ddb80eef0947be2
Dennison Bertram <dennison@dennisonbertram.com>
2020-12-30 14:41:29 -0500

    📦 NEW: Tutorial Code

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async () => {
@@ -11,6 +12,22 @@ task("accounts", "Prints the list of accounts", async () => {
   }
 });
 
+task("Deploy", "Deploys a COMPound style governance system")
+.addParam("token", "The address to receive the initial supply")
+.addParam("timelock", "The timelock administrator")
+.addParam("guardian", "The governor guardian").setAction(async taskArgs => {
+    
+  const { deploy } = require("./scripts/Deploy");
+
+    await deploy({
+      tokenRecipient: taskArgs.token,
+      timeLockAdmin: taskArgs.timelock,
+      guardian: taskArgs.guardian
+    });
+
+})
+
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
@@ -18,6 +35,12 @@ task("accounts", "Prints the list of accounts", async () => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  networks: {
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/<<Your infura Key>>",
+      accounts: ["0xAPrivateKey"]
+    }
+  },
   solidity: "0.5.16",
 };
 
--- scripts/Deploy.js
+++ scripts/Deploy.js
@@ -2,31 +2,27 @@
 const hre = require("hardhat");
 const ethers = hre.ethers;
 
-async function main() {
+async function main({tokenRecipient, timeLockAdmin, guardian}) {
 
     // Compile our Contracts, just in case
     await hre.run('compile');
 
-    // Get a signer from the HardHard environment
-    // Learn about signers here: https://docs.ethers.io/v4/api-wallet.html
-    const [tokenRecipient, timelockAdmin, guardian] = await ethers.getSigners();
-
     // This gets the contract from 
     const Token = await hre.ethers.getContractFactory("Comp");
-    const token = await Token.deploy(tokenRecipient.address);
+    const token = await Token.deploy(tokenRecipient);
     await token.deployed();
     await token.deployTransaction.wait();
     
     // Deploy Timelock
     const delay = 172800;
     const Timelock = await ethers.getContractFactory("Timelock");
-    const timelock = await Timelock.deploy(timelockAdmin.address, delay);
+    const timelock = await Timelock.deploy(timeLockAdmin, delay);
     await timelock.deployed();
     await timelock.deployTransaction.wait();
 
     // Deploy Governance
     const Gov = await ethers.getContractFactory("GovernorAlpha");
-    const gov = await gov.deploy(timelock.address, token.address, guardian.address);
+    const gov = await Gov.deploy(timelock.address, token.address, guardian);
     await gov.deployed();
     await gov.deployTransaction.wait();
 
@@ -34,13 +30,10 @@ async function main() {
     console.log(`TimeLock deployed to: ${timelock.address}`);
     console.log(`GovernorAlpha deployed to: ${gov.address}`)
 
-    const initialBalance = await token.balanceOf(tokenRecipient.address);
-    console.log(`${initialBalance / 1e18} tokens transfered to ${tokenRecipient.address}`);
+    const initialBalance = await token.balanceOf(tokenRecipient);
+    console.log(`${initialBalance / 1e18} tokens transfered to ${tokenRecipient}`);
 }
 
-main()
-    .then(() => process.exit(0))
-    .catch(error => {
-        console.error(error);
-        process.exit(1);
-    });
+module.exports = {
+        deploy: main
+    }
